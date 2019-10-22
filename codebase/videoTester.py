3030e69263a3cc0f84a207fb43f9c305d0620566
Konrad Wiley <konrad_f22@hotmail.com>
2019-10-22 17:08:49 -0400

    Added imageio and moviepy libraries to repository, and cleaned up videoTester.py

--- codebase/videoTester.py
+++ codebase/videoTester.py
@@ -1,50 +1,53 @@
-#requires the installation of moviepy
+#requires the installation of moviepy and imagemagick
 from moviepy.video.tools.subtitles import SubtitlesClip
 from moviepy.editor import*
 import subprocess
-import requests
-from giphypop import Giphy
+
+from giphypop import Giphy  # necessary for uploading to giphy.com
+import webbrowser  # used to open giphy.com URL after upload
+from datetime import datetime  # used to create filename of gif in this context
 
 
 #globals that will need defined for gifEngine to work
-outfile = r"G:\Users\Tempest3\Documents\USCU\Fall 2019\Software Engineering\QuoteGIFr\outfile"
-gifNum = 5
 
 
+#Giphy api_key, required for uploads
 API_KEY = 'iTmKRrpWJUCpn6nWMSIp42gmkXA6hpfh'
 
 #the four variables defined below are what gifEngine needs
 def main():
-	
-	startTime = "00:04:49,934"
-	endTime = "00:05:01,500"
-	videoFileLoc = r"G:\Users\Tempest3\Documents\USCU\Fall 2019\Software Engineering\QuoteGIFr\MEDIA\GS_S01E01.mp4"
-	strFileLoc = r"G:\Users\Tempest3\Documents\USCU\Fall 2019\Software Engineering\QuoteGIFr\MEDIA\GS_S01E01.srt"
-	
-	print(gifEngine(startTime, endTime, videoFileLoc, strFileLoc) + " rendered successfully")
-	
-	global gifNum
-	uploadLoc = outfile + "\GIF_%d.gif"%gifNum
-	giphyobj = Giphy(API_Key)
-	response = giphyobj.upload("Get Smart, Cone of Silence", uploadLoc, username="konradwiley")
+    
+    starttime = "00:04:49,934"
+    endtime = "00:05:01,500"
+    # set these to appropriate locations, eventually will be passed in from db
+    videofileloc = r"G:\Users\Tempest3\Documents\USCU\Fall 2019\Software Engineering\QuoteGIFr\MEDIA\GS_S01E01.mp4"
+    strfileloc = r"G:\Users\Tempest3\Documents\USCU\Fall 2019\Software Engineering\QuoteGIFr\MEDIA\GS_S01E01.srt"
+    outfile = r"G:\Users\Tempest3\Documents\USCU\Fall 2019\Software Engineering\QuoteGIFr\outfile"
+    
+    outfileloc = (outfile+"\GIF_{}.gif").format(datetime.now().strftime("%H_%M_%S"))
 
-	print(response)  # gives us the url of GIF on giphy.com
-	
-	gifNum = gifNum + 1
+    print(gifEngine(starttime, endtime, videofileloc, strfileloc,  outfileloc) + " rendered successfully")
+    print("Uploading to giphy.com...")
+    
+    giphyobj = Giphy(API_KEY)
+    response = giphyobj.upload("Get Smart, Cone of Silence", outfileloc, username="QuoteGIFr")
+
+    webbrowser.open_new(str(response))  # shows us the GIF on giphy.com
+    
 
 #creates a gif from the videoFileLoc with subtitles from strFileLoc 
 #returns the location of the GIF as a string
-def gifEngine(startTime, endTime, videoFileLoc, srtFileLoc):
-	
-	#creating the initial GIF
-	generator = lambda txt: TextClip(txt, font='Impact', fontsize=36, color='white')
-	video = VideoFileClip(videoFileLoc)
-	sub = SubtitlesClip(srtFileLoc, generator).set_position(("center", "bottom"), relative=True)	
-	composite = CompositeVideoClip([video, sub])
-	composite = composite.subclip(startTime, endTime)
-	composite.write_gif(outfile + "\GIF_%d.gif"%gifNum, program='ffmpeg')
-	
-	return (outfile + "\GIF_%d.gif"%gifNum)	
-	
+def gifEngine(starttime, endtime, videofileloc, srtfileloc, outfileloc):
+    
+    #creating the initial GIF
+    generator = lambda txt: TextClip(txt, font='Impact', fontsize=28, color='white')
+    video = VideoFileClip(videofileloc)
+    sub = SubtitlesClip(srtfileloc, generator).set_position(("center", "bottom"), relative=True)	
+    composite = CompositeVideoClip([video, sub])
+    composite = composite.subclip(starttime, endtime)
+    composite.write_gif(outfileloc, program='ffmpeg', opt='palettegen')
+    
+    return (outfileloc)	
+    
 if '__main__' == __name__:
     main()
