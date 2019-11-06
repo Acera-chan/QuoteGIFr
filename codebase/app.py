b7ac072fb6ae34ef52fb9e588148d78d81029c96
Eli Hughes <roadtrip.adventure@outlook.com>
2019-11-06 16:26:03 -0500

    Added more comments to app.py and forms.py

--- codebase/app.py
+++ codebase/app.py
@@ -1,5 +1,4 @@
 # Author: Eli Hughes
-# Date Last Modified: 10/8/19
 # Purpose: Flask App for QuoteGIFr
 
 from flask import Flask, render_template, request, redirect, url_for
@@ -30,41 +29,47 @@ def homepage():
         for result in filmResults:
             filmForms.append([result, SelectMovieForm(movieName = result)])
         return render_template('index.html', form = form, filmRequest = filmRequest, filmForms = filmForms)
+
+    # if no movie request, display the base homepage
     return render_template('index.html', form = form)
 
 @app.route("/quote", methods = ["GET", "POST"])
 def quotepage():
+
+    # only allow access to the quotepage if the user selected a movie from "homepage" or they just searched for
+    # a quote using "quotepage"
     if request.method == 'POST':
         movieName = request.form.get('movieName')
         form = QuoteForm(movieName = movieName)
+
+        # if the user submitted a quote request, handle it
         if request.form.get('quote'):
             quoteRequest = request.form.get('quote')
+
+            # do db stuff here...
+            # the db query results using "quoteRequest"
             quoteResults = [quoteRequest]
+
+            # stores selection forms for all of the quote results the db returns
             quoteForms = []
             for result in quoteResults:
                 quoteForms.append([result, SelectQuoteForm(movieName = movieName, quote = result)])
             return render_template('quote.html', title = 'Quote', form = form, movieName = movieName, quoteRequest = quoteRequest, quoteForms = quoteForms)
         
+        # the user just came from "homepage"
         else:
             return render_template('quote.html', title = 'Quote', form = form, movieName = movieName)
 
+    # redirect the user if they didn't select a movie from "homepage" or search for a quote "quotepage" 
+    # (ie no post data submitted)
     return redirect(url_for('homepage'))
 
 @app.route("/generate", methods = ["GET", "POST"])
 def generateGIFpage():
+    # if post data submitted, print it out (will use this data to generate a gif)
     if request.method == 'POST':
         movie = request.form.get('movieName')
         quote = request.form.get('quote')
-
-        if movie == 'An Ideal Husband 1947' and quote == 'Surely. You have nothing to conceal?':
-            starttime = '00:42:16,160'
-            endtime = '00:42:18,036'
-
-            videofileloc = r"G:\Users\Eli\Projects\QuoteGIFr_proj\An Ideal Husband 1947.mp4"
-            strfileloc = r"G:\Users\Eli\Projects\QuoteGIFr_proj\An Ideal Husband 1947.srt"
-            outfile = r"G:\Users\Eli\Projects\QuoteGIFr_proj\outfile"
-
-            
         return render_template('generate.html', movie = movie, quote = quote)
 
     return redirect(url_for('homepage'))
--- codebase/forms.py
+++ codebase/forms.py
@@ -1,20 +1,28 @@
+# Author: Eli Hughes
+# Purpose: Form classes from which form objects will be made in "app.py"
+#          and presented in "index.html", and "quote.html"
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, HiddenField
 from wtforms.validators import DataRequired
 
+# class for a form for searching for a movie
 class MovieForm(FlaskForm):
     movieName = StringField('Movie Name', validators = [DataRequired()])
     submit = SubmitField('Search for this film')
 
+# class for a form for searching for a quote
 class QuoteForm(FlaskForm):
     movieName = HiddenField()
     quote = StringField('Film Quote', validators = [DataRequired()])
     submit = SubmitField('Search for this quote')
 
+# class for a form for selecting an individual movie
 class SelectMovieForm(FlaskForm):
     movieName = HiddenField()
     submit = SubmitField('Select')
 
+# class for a form for selecting an individual quote
 class SelectQuoteForm(FlaskForm):
     movieName = HiddenField()
     quote = HiddenField()
