7a4b47b632495fa263204feb338558649a5e00a8
Eli Hughes <roadtrip.adventure@outlook.com>
2019-11-06 17:08:13 -0500

    Added more comments to app.py

--- codebase/app.py
+++ codebase/app.py
@@ -14,37 +14,45 @@ def homepage():
     # form for user to enter a movie name they come up with
     form = MovieForm()
 
-    # if the user submitted a movie request, handle it
+    # if post data is submitted to this route, check it
     if request.method == 'POST':
-        # the user's film request
-        filmRequest = request.form.get('movieName')
+        # check to see if form data under the name of 'movieName' was submitted
+        if request.form.get('movieName'):
+            # the user's film request
+            filmRequest = request.form.get('movieName')
 
-        # do db stuff here...
-        # the db query results using "filmRequest"
-        filmResults = [filmRequest]
+            # do db stuff here...
+            # the db query results using "filmRequest"
+            filmResults = [filmRequest]
 
-        # stores selection forms for all of the movie results the db returns
-        filmForms = []
-        # create the selection forms
-        for result in filmResults:
-            filmForms.append([result, SelectMovieForm(movieName = result)])
-        return render_template('index.html', form = form, filmRequest = filmRequest, filmForms = filmForms)
+            # stores selection forms for all of the movie results the db returns
+            filmForms = []
+            # create the selection forms
+            for result in filmResults:
+                filmForms.append([result, SelectMovieForm(movieName = result)])
+            return render_template('index.html', form = form, filmRequest = filmRequest, filmForms = filmForms)
 
-    # if no movie request, display the base homepage
+    # if no post data (or satisfactory post data) was submitted, render the base homepage
     return render_template('index.html', form = form)
 
 @app.route("/quote", methods = ["GET", "POST"])
 def quotepage():
 
+    # !IMPORTANT! 
     # only allow access to the quotepage if the user selected a movie from "homepage" or they just searched for
     # a quote using "quotepage"
-    if request.method == 'POST':
-        movieName = request.form.get('movieName')
-        form = QuoteForm(movieName = movieName)
 
-        # if the user submitted a quote request, handle it
-        if request.form.get('quote'):
+    # if post data is submitted to this route, check it 
+    if request.method == 'POST':
+
+        # check to see if form data under the name of 'movieName' and 'quote' was submitted
+        if request.form.get('movieName') and request.form.get('quote'):
+
+            movieName = request.form.get('movieName')
             quoteRequest = request.form.get('quote')
+
+            # form for submitting a different quote request (search bar, essentially)
+            form = QuoteForm(movieName = movieName)
 
             # do db stuff here...
             # the db query results using "quoteRequest"
@@ -55,13 +63,20 @@ def quotepage():
             for result in quoteResults:
                 quoteForms.append([result, SelectQuoteForm(movieName = movieName, quote = result)])
             return render_template('quote.html', title = 'Quote', form = form, movieName = movieName, quoteRequest = quoteRequest, quoteForms = quoteForms)
-        
-        # the user just came from "homepage"
-        else:
+
+        # if form data under the names of 'movieName' and 'quote' were not both submitted, check to see if 
+        # just 'movieName' data was submitted (ie just came from "homepage")
+        elif request.form.get('movieName'):
+            movieName = request.form.get('movieName')
+
+            # form for submitting a quote request (search bar, essentially)
+            form = QuoteForm(movieName = movieName)
+
+            movieName = request.form.get('movieName')
             return render_template('quote.html', title = 'Quote', form = form, movieName = movieName)
 
-    # redirect the user if they didn't select a movie from "homepage" or search for a quote "quotepage" 
-    # (ie no post data submitted)
+    # redirect the user if they didn't select a movie from "homepage" or search for a quote on "quotepage" 
+    # (ie no correct post data submitted)
     return redirect(url_for('homepage'))
 
 @app.route("/generate", methods = ["GET", "POST"])
